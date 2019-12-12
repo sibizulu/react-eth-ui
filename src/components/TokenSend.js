c6e0e9dc1ea35e09605d6b871f0f842138c0da34
sruthi23 <sruthi2525@gmail.com>
2019-12-12 12:39:29 +0530

    Format form style

--- src/components/TokenSend.js
+++ src/components/TokenSend.js
@@ -1,47 +1,53 @@
 import React from 'react'
 import { Field, reduxForm } from 'redux-form'
-import { Form, Button } from 'semantic-ui-react'
+import { Segment, Form, Button, Header, Divider } from 'semantic-ui-react'
 import { InputField } from 'react-semantic-redux-form'
+import * as config from '../config'
 
 const TokenSend = props => {
     const { handleSubmit, onSubmit } = props
     return (
-        <Form onSubmit={handleSubmit(onSubmit)}>
-            <Field
-                name="From"
-                type="string"
-                label="Sender wallet address"
-                component={InputField}
-                placeholder="Sender wallet address"
-                readOnly
-            />
-            <Field
-                name="Token"
-                type="string"
-                label="Token address"
-                component={InputField}
-                placeholder="Token address"
-                readOnly
-            />
-            <Field
-                name="To"
-                type="string"
-                label="Receiver wallet address"
-                component={InputField}
-                placeholder="Receiver wallet address"
-            />
+        <Segment color={config.COLOR}>
+            <Form onSubmit={handleSubmit(onSubmit)}>
+                <Header as="h4">Token Transfer</Header>
+                <Divider />
+                <Field
+                    name="From"
+                    type="string"
+                    label="From"
+                    component={InputField}
+                    placeholder="Sender wallet address"
+                    readOnly
+                />
+                <Field
+                    name="Token"
+                    type="string"
+                    label="Token address"
+                    component={InputField}
+                    placeholder="Token address"
+                    readOnly
+                />
+                <Field
+                    name="To"
+                    type="string"
+                    label="Receiver wallet address"
+                    component={InputField}
+                    placeholder="Receiver wallet address"
+                />
 
-            <Field
-                name="Amount"
-                type="string"
-                label="Amount"
-                component={InputField}
-                placeholder="amount"
-            />
-            <Button primary size="huge">
-                Send
-            </Button>
-        </Form>
+                <Field
+                    name="Amount"
+                    type="string"
+                    label="Amount"
+                    component={InputField}
+                    placeholder="amount"
+                />
+                <Form.Button
+                    content="Submit"
+                    color={config.BUTTON_ACTION_COLOR}
+                />
+            </Form>
+        </Segment>
     )
 }
 
